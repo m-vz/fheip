30648007f6eff53895d6f8064d475821703b122d
milan <milan@milan.wtf>
2023-06-25 14:56:16 +0200

    fix: support any number of png components

--- src/image.rs
+++ src/image.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::path::Path;
 
 use log::debug;
-use png::Decoder;
+use png::{ColorType, Decoder};
 use serde::{Deserialize, Serialize};
 use tfhe::shortint::{CiphertextBig, ClientKey};
 
@@ -23,30 +23,31 @@ impl Debug for Size {
 }
 
 #[derive(PartialEq, Eq, Serialize, Deserialize)]
-pub struct Image<T> {
-    pub data: Vec<T>,
-    pub size: Size,
+pub struct Image<T: Clone> {
+    data: Vec<T>,
+    size: Size,
+    components: u16,
 }
 
-impl<T> Image<T> {
-    pub fn new(data: Vec<T>, width: u16, height: u16) -> Self {
+impl<T: Clone> Image<T> {
+    pub fn new(data: Vec<T>, width: u16, height: u16, components: u16) -> Self {
         Self {
             data,
             size: Size { width, height },
+            components,
         }
     }
 
     pub fn get_pixel(&self, x: u16, y: u16) -> Option<Vec<&T>> {
-        let (x, y) = (x * 3, y * 3);
+        let (x, y) = (x * self.components, y * self.components);
         let index = (x + y * self.size.width) as usize;
+        let mut pixel = Vec::new();
 
-        vec![
-            self.data.get(index),
-            self.data.get(index + 1),
-            self.data.get(index + 2),
-        ]
-        .into_iter()
-        .collect::<Option<Vec<&T>>>()
+        for component in 0..self.components {
+            pixel.push(self.data.get(index + component as usize));
+        }
+
+        pixel.into_iter().collect::<Option<Vec<&T>>>()
     }
 }
 
@@ -62,6 +63,11 @@ impl PlaintextImage {
             buffer[..info.buffer_size()].to_vec(),
             info.width as u16,
             info.height as u16,
+            match info.color_type {
+                ColorType::Grayscale | ColorType::Indexed => 1,
+                ColorType::Rgb | ColorType::GrayscaleAlpha => 2,
+                ColorType::Rgba => 4,
+            },
         );
         debug!("Loaded {:?}", image);
 
@@ -77,6 +83,7 @@ impl PlaintextImage {
                 .map(|x| key.encrypt(*x as u64))
                 .collect(),
             size: self.size,
+            components: self.components,
         }
     }
 }
