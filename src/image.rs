9c55c74d2dddf434a75fff3b799f8961fafaf603
milan <milan@milan.wtf>
2023-06-25 14:44:18 +0200

    fix: return all components of pixel refactor: use u16 for image size

--- src/image.rs
+++ src/image.rs
@@ -3,14 +3,14 @@ use std::fmt::{Debug, Formatter};
 use std::fs::File;
 use std::path::Path;
 
-use log::{debug, trace};
+use log::debug;
 use png::Decoder;
 use serde::{Deserialize, Serialize};
 use tfhe::shortint::{CiphertextBig, ClientKey};
 
 pub mod rescaling;
 
-#[derive(PartialEq, Eq, Serialize, Deserialize)]
+#[derive(PartialEq, Eq, Serialize, Deserialize, Clone, Copy)]
 pub struct Size {
     pub width: u32,
     pub height: u32,
@@ -24,20 +24,29 @@ impl Debug for Size {
 
 #[derive(PartialEq, Eq, Serialize, Deserialize)]
 pub struct Image<T> {
-    data: Vec<T>,
-    size: Size,
+    pub data: Vec<T>,
+    pub size: Size,
 }
 
 impl<T> Image<T> {
-    pub fn new(data: Vec<T>, width: u32, height: u32) -> Self {
+    pub fn new(data: Vec<T>, width: u16, height: u16) -> Self {
         Self {
             data,
             size: Size { width, height },
         }
     }
 
-    pub fn get_pixel(&self, x: u32, y: u32) -> Option<&T> {
-        self.data.get((x + y * self.size.width) as usize)
+    pub fn get_pixel(&self, x: u16, y: u16) -> Option<Vec<&T>> {
+        let (x, y) = (x * 3, y * 3);
+        let index = (x + y * self.size.width) as usize;
+
+        vec![
+            self.data.get(index),
+            self.data.get(index + 1),
+            self.data.get(index + 2),
+        ]
+        .into_iter()
+        .collect::<Option<Vec<&T>>>()
     }
 }
 
@@ -51,11 +60,10 @@ impl PlaintextImage {
         let info = reader.next_frame(&mut buffer)?;
         let image = Image::new(
             buffer[..info.buffer_size()].to_vec(),
-            info.width,
-            info.height,
+            info.width as u16,
+            info.height as u16,
         );
         debug!("Loaded {:?}", image);
-        trace!("Image data: {:?}", image.data);
 
         Ok(image)
     }
@@ -68,17 +76,14 @@ impl PlaintextImage {
                 .iter_mut()
                 .map(|x| key.encrypt(*x as u64))
                 .collect(),
-            size: Size {
-                width: self.size.width,
-                height: self.size.height,
-            },
+            size: self.size,
         }
     }
 }
 
 impl Debug for PlaintextImage {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Image ({:?})", self.size)
+        write!(f, "Image ({:?}), data: {:?}", self.size, self.data)
     }
 }
 
