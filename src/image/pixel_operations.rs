9bc6f4f58a439630bd2ae82b2edc69d33c8e8e4d
milan <milan@milan.wtf>
2023-06-26 10:05:59 +0200

    fix: invert should leave alpha channel

--- src/image/pixel_operations.rs
+++ src/image/pixel_operations.rs
@@ -1,13 +1,38 @@
 use crate::crypt::ServerKeyType;
-use crate::image::{EncryptedImage, Image};
+use crate::image::{ColorType, EncryptedImage, Image};
 
 pub fn invert(image: &EncryptedImage, key: &ServerKeyType) -> EncryptedImage {
     Image::new(
-        image
-            .data
-            .iter()
-            .map(|x| key.neg_parallelized(&key.scalar_sub_parallelized(x, 255_u64)))
-            .collect(),
+        match image.color_type {
+            ColorType::GrayscaleAlpha | ColorType::Rgba => {
+                let mut inverted_data = Vec::with_capacity(
+                    (image.size.width * image.size.height * image.channel_count()) as usize,
+                );
+
+                for y in 0..image.size.height {
+                    for x in 0..image.size.width {
+                        let pixel = image.get_pixel(x, y).unwrap();
+                        let mut pixel = pixel.iter();
+
+                        // invert rgb/grayscale values
+                        (0..image.channel_count() - 1).for_each(|_| {
+                            inverted_data.push(key.neg_parallelized(
+                                &key.scalar_sub_parallelized(pixel.next().unwrap(), 255_u64),
+                            ))
+                        });
+                        // copy alpha value
+                        inverted_data.push((*pixel.next().unwrap()).clone());
+                    }
+                }
+
+                inverted_data
+            }
+            ColorType::Grayscale | ColorType::Indexed | ColorType::Rgb => image
+                .data
+                .iter()
+                .map(|x| key.neg_parallelized(&key.scalar_sub_parallelized(x, 255_u64)))
+                .collect(),
+        },
         image.size.width,
         image.size.height,
         image.color_type,
