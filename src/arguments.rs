6801cac8b7226932018f99053b7b921a7e9a560d
milan <milan@milan.wtf>
2023-06-26 09:22:33 +0200

    feat: make this a cli app

---
+++ src/arguments.rs
@@ -0,0 +1,51 @@
+use std::path::PathBuf;
+
+use clap::{ArgGroup, Args, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+pub struct Arguments {
+    /// A command to run
+    #[clap(subcommand)]
+    pub command: Command,
+    /// The server address
+    #[arg(short, long, default_value = "127.0.0.1:34347")]
+    pub address: String,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    /// Start a server
+    Server,
+    /// Ping the server
+    Ping,
+    /// Tell the server to shut down
+    Shutdown,
+    /// Send an image to the server
+    Load(LoadCommand),
+    /// Rescale the image stored on the server
+    Rescale(RescaleCommand),
+    /// Invert the image stored on the server
+    Invert,
+}
+
+#[derive(Debug, Args)]
+pub struct LoadCommand {
+    /// The path to the image
+    pub file: PathBuf,
+}
+
+#[derive(Debug, Args)]
+#[clap(group(ArgGroup::new("interpolation").required(true).args(&["bilinear", "nearest"])))]
+pub struct RescaleCommand {
+    /// Use bilinear interpolation
+    #[arg(long)]
+    pub bilinear: bool,
+    // Use nearest-neighbour interpolation
+    #[arg(long)]
+    pub nearest: bool,
+    /// The new width of the image
+    pub width: u16,
+    /// The new height of the image
+    pub height: u16,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::error::Error;
 use std::path::Path;
-use std::thread;
 
+use clap::Parser;
 use log::info;
 
+use crate::arguments::{Arguments, Command, LoadCommand};
 use crate::client::Client;
 use crate::crypt::key::load_or_generate_keys;
 use crate::image::rescaling::InterpolationType;
 use crate::image::{Image, Size};
 use crate::message::Message;
+use crate::server::Server;
 
+mod arguments;
 mod client;
 mod crypt;
 mod exploration;
@@ -17,43 +20,86 @@ mod image;
 mod message;
 mod server;
 
-const ADDRESS: &str = "127.0.0.1:34347";
-
 fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
 
-    let (client_key, server_key) =
-        load_or_generate_keys(Path::new("data/keys/client"), Path::new("data/keys/server"))?;
-    let join_handle = thread::spawn(|| server::Server::new(server_key).start(ADDRESS).unwrap());
-    let client = Client::new(ADDRESS, client_key);
-    let image = Image::load(Path::new("data/images/charmander-21x18.png"))?;
-    let encrypted_image = client.encrypt_image(&image);
+    let arguments = Arguments::parse();
+    let address = arguments.address;
 
-    client.send_message(Message::Image(encrypted_image))?;
-    let scale = (7, 6);
-    let interpolation_type = InterpolationType::Bilinear;
-    info!("Rescaling {}x{}...", scale.0, scale.1);
-    let answer = client.send_message(Message::Rescale(
-        Size {
-            width: scale.0,
-            height: scale.1,
-        },
-        interpolation_type,
-    ))?;
-    if let Some(Message::Image(image)) = answer {
-        let decrypted_image = client.decrypt_image(&image);
-        info!("Decrypted: {:?}", decrypted_image);
-        decrypted_image.save(Path::new(
-            format!(
-                "data/output/charmander-rescaled-{:?}-{}x{}.png",
-                interpolation_type, scale.0, scale.1
-            )
-            .as_str(),
-        ))?;
+    match arguments.command {
+        Command::Server => {
+            let (_, server_key) = load_or_generate_keys(
+                Path::new("data/keys/client"),
+                Path::new("data/keys/server"),
+            )?;
+            Server::new(server_key).start(address.as_str())?;
+        }
+        command => {
+            let (client_key, _) = load_or_generate_keys(
+                Path::new("data/keys/client"),
+                Path::new("data/keys/server"),
+            )?;
+            let client = Client::new(address.as_str(), client_key);
+
+            match command {
+                Command::Ping => {
+                    let answer = client.send_message(Message::Ping)?;
+                    if let Some(Message::Pong) = answer {
+                        info!("Server reached");
+                    }
+                }
+                Command::Shutdown => {
+                    client.send_message(Message::Shutdown)?;
+                }
+                Command::Load(LoadCommand { file }) => {
+                    let image = Image::load(file.as_path())?;
+
+                    client.send_message(Message::Image(client.encrypt_image(&image)))?;
+                }
+                Command::Rescale(rescale_command) => {
+                    let interpolation_type = if rescale_command.bilinear {
+                        InterpolationType::Bilinear
+                    } else {
+                        InterpolationType::Nearest
+                    };
+                    info!(
+                        "Rescaling {}x{}...",
+                        rescale_command.width, rescale_command.height
+                    );
+
+                    let answer = client.send_message(Message::Rescale(
+                        Size {
+                            width: rescale_command.width,
+                            height: rescale_command.height,
+                        },
+                        interpolation_type,
+                    ))?;
+                    if let Some(Message::Image(image)) = answer {
+                        let decrypted_image = client.decrypt_image(&image);
+                        info!("Decrypted: {:?}", decrypted_image);
+
+                        decrypted_image.save(Path::new(
+                            format!(
+                                "data/output/rescaled-{:?}-{}x{}.png",
+                                interpolation_type, rescale_command.width, rescale_command.height
+                            )
+                            .as_str(),
+                        ))?;
+                    }
+                }
+                Command::Invert => {
+                    let answer = client.send_message(Message::Invert)?;
+                    if let Some(Message::Image(image)) = answer {
+                        let decrypted_image = client.decrypt_image(&image);
+                        info!("Decrypted: {:?}", decrypted_image);
+
+                        decrypted_image.save(Path::new("data/output/inverted.png"))?;
+                    }
+                }
+                Command::Server => unreachable!(),
+            }
+        }
     }
-
-    client.send_message(Message::Shutdown)?;
-    join_handle.join().unwrap();
 
     Ok(())
 }
